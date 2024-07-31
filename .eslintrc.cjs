11268e2f932d9ca0b658cfa849b20a3fdfd2afbc
jhigger <njohnjhigger@gmail.com>
2024-08-01 00:38:19 +0800

    setup boilerplate

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,9 +1,11 @@
 module.exports = {
   extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    '@electron-toolkit/eslint-config-ts/recommended',
-    '@electron-toolkit/eslint-config-prettier'
-  ]
-}
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "@electron-toolkit/eslint-config-ts/recommended",
+    "@electron-toolkit/eslint-config-prettier",
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "plugin:@typescript-eslint/stylistic-type-checked",
+  ],
+};
--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,35 +1,43 @@
-import Versions from './components/Versions'
-import electronLogo from './assets/electron.svg'
+import { Minus, Plus } from "lucide-react";
+import electronLogo from "./assets/electron.svg";
+import { Button } from "./components/ui/button";
+import { useState } from "react";
+import { toast } from "sonner";
 
 function App(): JSX.Element {
-  const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
+  const [count, setCount] = useState(0);
+
+  const increment = () =>
+    setCount((count) => {
+      toast("Add", {
+        description: "Increased by 1",
+      });
+      return count + 1;
+    });
+
+  const decrement = () =>
+    setCount((count) => {
+      toast("Subtract", {
+        description: "Decreased by 1",
+      });
+      return count - 1;
+    });
 
   return (
-    <>
-      <img alt="logo" className="logo" src={electronLogo} />
-      <div className="creator">Powered by electron-vite</div>
-      <div className="text">
-        Build an Electron app with <span className="react">React</span>
-        &nbsp;and <span className="ts">TypeScript</span>
+    <main className="dark flex min-h-screen w-screen flex-col items-center justify-center gap-4 bg-zinc-950">
+      <h1 className="text-4xl text-white">Hello, World!</h1>
+      <img src={electronLogo} alt="logo" className="size-40 animate-spin" />
+      <div className="flex items-center gap-8">
+        <Button size="icon" onClick={decrement}>
+          <Minus />
+        </Button>
+        <span className="pb-2 text-7xl text-white">{count}</span>
+        <Button size="icon" onClick={increment}>
+          <Plus />
+        </Button>
       </div>
-      <p className="tip">
-        Please try pressing <code>F12</code> to open the devTool
-      </p>
-      <div className="actions">
-        <div className="action">
-          <a href="https://electron-vite.org/" target="_blank" rel="noreferrer">
-            Documentation
-          </a>
-        </div>
-        <div className="action">
-          <a target="_blank" rel="noreferrer" onClick={ipcHandle}>
-            Send IPC
-          </a>
-        </div>
-      </div>
-      <Versions></Versions>
-    </>
-  )
+    </main>
+  );
 }
 
-export default App
+export default App;
